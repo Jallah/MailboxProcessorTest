5f35be750df639d727e22f585d04bea9b2e96cbf
Jallah <puncher@gmx.net>
2016-10-08 19:47:25 +0200

    inital commit

---
+++ Client/Program.fs
@@ -0,0 +1,40 @@
+﻿let Agent =
+    new MailboxProcessor<string>(fun inbox ->
+
+                let tcpClient = new System.Net.Sockets.TcpClient()
+
+                tcpClient.Connect("127.0.0.1", 8888)
+
+                let streamWriter = new System.IO.StreamWriter(tcpClient.GetStream())
+
+                let rec loop() =
+
+                        async {
+
+                            let! msg = inbox.Receive()
+
+                            do! streamWriter.WriteLineAsync(msg) |> Async.AwaitTask
+
+                            do! streamWriter.FlushAsync() |> Async.AwaitTask
+
+                            printfn "send msg %s" msg
+
+                            return! loop()
+                        }
+                loop())
+
+           
+
+[<EntryPoint>]
+
+let main argv =
+
+    Agent.Start()
+
+    while true do
+
+        let msg = System.Console.ReadLine()
+
+        Agent.Post(msg)
+
+    0
---
+++ Server/Program.fs
@@ -0,0 +1,72 @@
+﻿
+type client = { id:int; connection:System.Net.Sockets.TcpClient }
+
+let getStreamReaderAgent() =
+    new MailboxProcessor<client>(fun inbox ->
+
+        async {
+            let! client = inbox.Receive()
+
+            let streamReader = new System.IO.StreamReader(client.connection.GetStream())
+
+            while true do
+
+                let! msg = streamReader.ReadLineAsync() |> Async.AwaitTask
+
+                printfn "got msg from %i: %s" client.id msg
+        })
+
+
+let connectionHandler =
+    new MailboxProcessor<System.Net.Sockets.TcpClient>(fun inbox ->
+    
+    let rec loop id clients=
+        async {
+            let! con = inbox.Receive()
+
+            let client = {id=id; connection=con}
+
+            let readerAgent = getStreamReaderAgent()
+
+            readerAgent.Start()
+
+            readerAgent.Post client
+
+            return! loop (id + 1) ([client] @ clients)
+        }
+
+    loop 0 [])
+
+
+let connectionListener =
+    new MailboxProcessor<unit>(fun _ ->
+
+        let listener = new System.Net.Sockets.TcpListener(System.Net.IPAddress.Parse("0.0.0.0"), 8888)
+
+        connectionHandler.Start()
+
+        listener.Start()
+
+        let rec listenLoop() =
+
+            async{
+                let! client = listener.AcceptTcpClientAsync() |> Async.AwaitTask
+
+                connectionHandler.Post client
+
+                printfn "client connected: %s" (client.Client.LocalEndPoint.ToString())
+
+                return! listenLoop()
+            }
+        listenLoop())
+ 
+
+[<EntryPoint>]
+
+let main _ =
+
+    connectionListener.Start()
+
+    System.Console.ReadLine() |> ignore
+
+    0
